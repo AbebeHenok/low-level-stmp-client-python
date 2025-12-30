a5a8509d4a040cfef09c27c95f5aecf9f08c6c26
Henok Abebe <154027504+AbebeHenok@users.noreply.github.com>
2025-12-29 23:35:08 -0500

    Refactor email sending functionality
    Updated email subject and message input handling. Changed environment variable names for SMTP authentication and updated recipient email address.

--- smtpClient.py
+++ smtpClient.py
@@ -3,8 +3,12 @@ import base64
 from ssl import SSLContext
 import ssl
 import os
+
+subject = "SUBJECT: " +input("Enter the subject of the email:")
+subject += "\r\n"
+msg = input("Enter the message you want to send: ")
 # Change the message to the desired one.
-msg = "\r\n This is my message. I am sending this email using Python SMTP client."
+# msg = "\r\n This is my message. I am sending this email using Python SMTP client."
 endmsg = "\r\n.\r\n"
 
 ## Mail server and port ##
@@ -42,8 +46,10 @@ if recv2[:3] != '220':
     print("220 reply not received after STARTTLS.")
 
 #We must authenticate the user sending the email.
-username = os.environ.get('EMAIL_USER')
-password = os.environ.get('EMAIL_PASS')
+# username = "youremail@gmail.com"
+# password = "yourpassword"
+username = os.environ.get('SMTP_USERNAME')
+password = os.environ.get('SMTP_PASSWORD')
 base64_str = ("\x00"+username+"\x00"+password).encode()
 base64_str = base64.b64encode(base64_str)
 base64_str = base64_str.strip("\n".encode())
@@ -54,21 +60,21 @@ print(recv_auth)
 
 
 #Send MAIL FROM command and print server response
-mailFrom = 'MAIL FROM: <youremail@gmail.com>\r\n'
+mailFrom = 'MAIL FROM: <{username}>\r\n'
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 print("After MAIL FROM command: " +recv2.decode())
 if recv2[:3] != '250':
         print("250 reply not received from server from RECV2")
 
-
 #Send RCPT TO command and print server response
 # Change the receiver email address to the desired one.
-rcpt = 'RCPT TO: <receiver@gmail.com>\r\n'
+
+rcpt = 'RCPT TO: <henok.abebe.ce@gmail.com>\r\n'
 clientSocket.send(rcpt.encode())
 recv3 = clientSocket.recv(1024).decode()
 print("After RCPT TO command: " +recv3)
-if recv[:3] != '250':
+if recv3[:3] != '250':
         print("250 reply not received from server FROM RECV3")
 
 #Send DATA command and print server respose. 
@@ -81,7 +87,7 @@ if recv4[:3] != '250':
 
 #Send message data
 #Change the subject line to the desired one.
-subject = "SUBJECT: Your Subject\r\n"
+# subject = "SUBJECT: Your Subject\r\n"
 clientSocket.send(subject.encode())
 msgData = msg + endmsg
 clientSocket.send(msgData.encode())
@@ -93,7 +99,7 @@ quitCommand = 'QUIT\r\n'
 clientSocket.send(quitCommand.encode())
 recv6 = clientSocket.recv(1024).decode()
 print(recv6)
-if recv1[:3] != '250':
+if recv6[:3] != '250':
     print( '250 reply not received from server.' )
 #Closing the TCP connection.
 clientSocket.close()
